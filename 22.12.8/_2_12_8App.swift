987f2a3e3302fe0df063239fa12683571b27fd62
chosangwoo <kajer102@gmail.com>
2022-12-08 14:22:51 +0900

    Initial Commit

---
+++ 22.12.8/_2_12_8App.swift
@@ -0,0 +1,20 @@
+//
+//  _2_12_8App.swift
+//  22.12.8
+//
+//  Created by 조상우 on 2022/12/08.
+//
+
+import SwiftUI
+
+@main
+struct _2_12_8App: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
